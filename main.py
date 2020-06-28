74c0c664f2375d04a7af1c6fca6126bfa251ff65
Eli Vaughn <eli@steenbean.local>
2020-06-28 08:08:27 -0500

    add Board and Square classes

--- main.py
+++ main.py
@@ -1,18 +1,61 @@
 #!/usr/bin/env python3
 
-def yes_or_no(question):
-    while "the answer is invalid":
-        reply = input(question+' (y/n): ').lower().strip()
-        if reply[:1] == 'y':
-            return True
-        if reply[:1] == 'n':
-            return False
+class Board:
+    size = 9
+    def __init__(self):
+        self.board = []
 
-def welcome():
-    print("Let's Play Tic-Tac-Toe!\n")
-    going_first = yes_or_no("Are you going first?")
-    print(going_first)
+        for _ in range(Board.size):
+            self.board.append(Square())
 
+    def render(self):
+        for i, s in enumerate(self.board):
+            if i % 3 == 0:
+                print()
+            print(s.display() + " ", end="")
+        print()
+
+    def set(self, square_num, xo):
+        i = square_num - 1
+        self.board[i].state = xo
+
+class Square:
+    def __init__(self, state=None):
+        self.state = state
+
+    def display(self):
+        if self.state: 
+            return self.state
+        else:
+            return "-"
+
+    def __str__(self):
+        return self.display()
+
+    def __repr__(self):
+        return f"<Square state:{self.display()}>"
+
+def main():
+    b = Board()
+    b.render()
+    b.set(5, 'o')
+    b.render()
+    b.set(7, 'o')
+    b.render()
+    b.set(1, 'x')
+    b.render()
+    b.set(9, 'x')
+    b.render()
+    b.set(2, 'x')
+    b.render()
+    b.set(4, 'x')
+    b.render()
+    b.set(6, 'x')
+    b.render()
+    b.set(8, 'x')
+    b.render()
+    b.set(3, 'o')
+    b.render()
 
 if __name__ == '__main__':
-    welcome()
+    main()
