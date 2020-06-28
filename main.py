b8c5e5062f8b9c1ae3c35efd28e49f89e69d7a0c
Chris Vaughn <chris.vaughn@life.church>
2020-06-28 09:27:15 -0500

    add basic game play logic, improved render

--- main.py
+++ main.py
@@ -10,31 +10,44 @@ class Board:
         for _ in range(self.rows * self.columns):
             self.board.append(Square())
 
-    def render(self):
+    def render(self, show_square_num=False):
         for i, s in enumerate(self.board):
-            if i % self.columns == 0:
+            if i != 0 and i % self.columns == 0:
                 print()
-            print(s.display() + " ", end="")
-        print()
+                print("-----+-----+-----")
+            if show_square_num:
+                d = f"  {s.display(default=str(i+1))}  "
+            else:
+                d = f"  {s.display()}  "
+            if i % self.columns != 2:
+                d += "|"
+            print(d, end="")
+        print("\n")
 
     def set(self, square_num, xo):
         i = square_num - 1
         self.board[i].state = xo
 
+    def game_over(self):
+        for s in self.board:
+            if s.state is None:
+                return False
+        return True
+
 
 class Square:
     X = "X"
     O = "O"
-    EMPTY = "-"
+    EMPTY = " "
 
     def __init__(self, state=None):
         self.state = state
 
-    def display(self):
+    def display(self, default=EMPTY):
         if self.state:
             return self.state
         else:
-            return Square.EMPTY
+            return default
 
     def __str__(self):
         return self.display()
@@ -43,27 +56,38 @@ class Square:
         return f"<Square state:{self.display()}>"
 
 
+def valid_mark(player, turn):
+    while "the answer is invalid":
+        reply = input(f"{player} place a {turn} in square [1-9]: ").lower().strip()
+        try:
+            move = int(reply)
+        except ValueError:
+            move = 0
+        if 1 <= move <= 9:
+            return move
+
+
 def main():
+    print("Welcome to Tic Tac Toe!")
+    print("Player 1 will be X, player 2 will be O")
+    print()
     b = Board()
-    b.render()
-    b.set(5, Square.O)
-    b.render()
-    b.set(7, Square.O)
-    b.render()
-    b.set(1, Square.X)
-    b.render()
-    b.set(9, Square.X)
-    b.render()
-    b.set(2, Square.X)
-    b.render()
-    b.set(4, Square.X)
-    b.render()
-    b.set(6, Square.X)
-    b.render()
-    b.set(8, Square.X)
-    b.render()
-    b.set(3, Square.O)
-    b.render()
+    print("Here's the numbering system")
+    b.render(show_square_num=True)
+    turn = Square.X
+    while not b.game_over():
+        if turn == Square.X:
+            player = "Player 1"
+        else:
+            player = "Player 2"
+        square_num = valid_mark(player, turn)
+        b.set(square_num, turn)
+        b.render()
+        if turn == Square.X:
+            turn = Square.O
+        else:
+            turn = Square.X
+    print("Game Over!")
 
 
 if __name__ == "__main__":
